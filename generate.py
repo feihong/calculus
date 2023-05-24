6399f092525e644a96b1b2b083952df1b5a6b3ec
Feihong Hsu <feihong.hsu@gmail.com>
2023-05-24 18:28:36 -0500

    Add MathJax to generated pages

--- generate.py
+++ generate.py
@@ -25,6 +25,19 @@ HTML_BOILERPLATE = """\
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Python Note</title>
+  <script>
+  MathJax = {
+    tex: {
+      inlineMath: [['$', '$']]
+    },
+    svg: {
+      fontCache: 'global'
+    }
+  };
+  </script>
+  <script type="text/javascript" id="MathJax-script" async
+    src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
+  </script>
 </head>
 <body>
 """
