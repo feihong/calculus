a49cacb035a4fc1bae5102a73dc2d986da27c9a4
Feihong Hsu <feihong.hsu@gmail.com>
2023-05-31 19:03:39 -0500

    Simplify tokenize logic

--- pymd.py
+++ pymd.py
@@ -34,34 +34,54 @@ class Markdown:
 @dataclass
 class Code:
   lines: List[str]
+  finalized: bool
 
-  def add(self, lines):
+  def __init__(self, lines):
+    self.lines = lines
+    self.finalized = False
+    self.check_finalized()
+
+  def add(self, code):
     self.lines.append('')
-    self.lines.extend(lines)
+    self.lines.extend(code.lines)
+    self.check_finalized()
+
+  def check_finalized(self):
+    if self.lines[-1] == '#':
+      self.finalized = True
+      self.lines.pop()
 
 class Tokenizer:
-  last_token = None
-
   @staticmethod
   def tokenize(lines):
+    last_code = None
+
+    for token in Tokenizer._tokenize(lines):
+      match token:
+        case Code(_) as code:
+          if last_code is not None:
+            last_code.add(code)
+          else:
+            last_code = code
+        case _:
+          if last_code is not None:
+            yield last_code
+
+          yield token
+          last_code = None
+
+    if last_code is not None:
+      yield last_code
+
+  @staticmethod
+  def _tokenize(lines):
     for line in lines:
       if line == '':
         continue
       elif match := re.match(r'^(f?)"""', line):
-        if isinstance(Tokenizer.last_token, Code): yield Tokenizer.last_token
-        Tokenizer.last_token = Tokenizer.markdown(match.group(1) == 'f', lines)
-        yield Tokenizer.last_token
+        yield Tokenizer.markdown(match.group(1) == 'f', lines)
       else:
-        token = Tokenizer.code_or_header(line, lines)
-        match token:
-          case Header(_) as header:
-            if isinstance(Tokenizer.last_token, Code): yield Tokenizer.last_token
-            Tokenizer.last_token = token
-            yield token
-          case Code(_) as code:
-            Tokenizer.last_token = code
-
-    if isinstance(Tokenizer.last_token, Code): yield Tokenizer.last_token
+        yield Tokenizer.code_or_header(line, lines)
 
   @staticmethod
   def markdown(is_fstring: bool, lines: List[str]):
@@ -88,12 +108,7 @@ class Tokenizer:
     if len(accumulator) == 1 and accumulator[0].startswith('#'):
       return Header.from_line(accumulator[0])
     else:
-      match Tokenizer.last_token:
-        case Code(_) as code:
-          code.add(accumulator)
-          return None
-        case _:
-          return Code(accumulator)
+      return Code(accumulator)
 
 def printable(s):
     return s.replace('\\', '\\\\').replace('\n', '\\n').replace('"', '\\"')
@@ -131,3 +146,9 @@ def convert_to(input_file: Path, output_file: Path):
   with output_file.open('w', encoding='utf8') as fp:
     for chunk in get_code_chunks(tokens):
       fp.write(chunk + '\n')
+
+
+if __name__ == '__main__':
+  tokens = Tokenizer.tokenize(get_lines(Path('functions/07-discontinuities.py')))
+  for token in tokens:
+    print(token)
